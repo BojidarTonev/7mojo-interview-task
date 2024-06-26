227e766706c418c5c64fe74433835f6e5e55efee
Bojidar <bojidar.tonev.9@gmail.com>
2024-06-26 14:11:49 +0300

    finished restructoring of fe components

--- 7mojo-interview-task/src/components/casino-games-wrapper/CasinoGamesWrapper.tsx
+++ 7mojo-interview-task/src/components/casino-games-wrapper/CasinoGamesWrapper.tsx
@@ -1,6 +1,9 @@
 import {IGameType} from "../../types/types/game-types";
 import HorizontalSeparator from "../horizontal-seperator/HorizontalSeparator";
 import GameSlots from "../game-slots/GameSlots";
+import {useEffect, useState} from "react";
+import {ILabelValuePair} from "../../types/common";
+import {useAppSelector} from "../../redux/store";
 import './CasinoGamesWrapper.scss';
 
 interface ICasinoGamesWrapperProps {
@@ -12,14 +15,54 @@ interface ICasinoGamesWrapperProps {
     isLoading: boolean
 }
 const CasinoGamesWrapper = (props: ICasinoGamesWrapperProps) => {
-    const { name, games, isFeatured = false, hasFilter = false, areSlotGames = false, isLoading } = props;
+    const { name, games = [], isFeatured = false, hasFilter = false, areSlotGames = false, isLoading } = props;
+    const {selectedGameFeatures, selectedLines} = useAppSelector((state) => state.gamesFilters.slotGamesFilters);
+
+    const [displayGames, setDisplayGames] = useState<IGameType[]>([]);
+
+    useEffect(() => {
+        if (isFeatured) {
+            const filteredFeaturedGamesData: IGameType[] = games?.filter((el: IGameType) => el.isFeatured);
+            setDisplayGames(filteredFeaturedGamesData)
+        } else if (hasFilter) {
+            const [minimumLines, maximumLines] = getSelectedLineValues(selectedLines);
+            const filteredAllGamesData = filterAllGamesData(games, maximumLines, minimumLines, selectedGameFeatures);
+
+            setDisplayGames(filteredAllGamesData);
+        } else {
+            setDisplayGames(games);
+        }
+    }, [isFeatured, hasFilter, games, selectedGameFeatures, selectedLines]);
 
     return(<>
         <HorizontalSeparator name={name} hasFilter={hasFilter} />
         {isLoading ?
             <>Loading games...</> :
-            <GameSlots games={games} areSlotGames={areSlotGames} isFeatured={isFeatured} isFilterable={hasFilter} />}
+            <GameSlots games={displayGames} areSlotGames={areSlotGames} isFeatured={isFeatured} isFilterable={hasFilter} />}
     </>)
 }
 
 export default CasinoGamesWrapper;
+
+// these functions are currently staying in this file since they are not reused anywhere else,
+// so having them here would keep the code base cleaner
+const filterAllGamesData = (allGamesData: IGameType[], maximumLines: number, minimumLines: number, selectedGameFeatures: ILabelValuePair[]) => {
+    return allGamesData?.filter((el: IGameType) =>
+        el.slotData.linesCount >= minimumLines &&
+        el.slotData.linesCount <= maximumLines &&
+        (selectedGameFeatures.length === 0 || el.slotData.tags.some(tag => selectedGameFeatures.map(gf => gf.value).includes(tag))));
+}
+
+const getSelectedLineValues = (selectedLines: string[]): [number, number] => {
+    const selectedLineValues = selectedLines[0]
+        ? selectedLines[0].split(/[->]/).filter(el => !isNaN(el)).filter(el => el).map(el => Number(el))
+        : [0, Infinity];
+    // in case we have >50 to keep the logic flow without braking it
+    if (selectedLineValues.length === 1) {
+        selectedLineValues.push(Infinity);
+    }
+    const minimumLines = selectedLineValues[0];
+    const maximumLines = selectedLineValues[1];
+
+    return [minimumLines, maximumLines];
+}
--- 7mojo-interview-task/src/pages/slot-games/SlotGamesPage.tsx
+++ 7mojo-interview-task/src/pages/slot-games/SlotGamesPage.tsx
@@ -1,8 +1,5 @@
-import {useEffect, useState} from "react";
 import {useAppSelector} from "../../redux/store";
 import {useGetGamesQuery} from "../../redux/services/games-api";
-import {IGameType} from "../../types/types/game-types";
-import {ILabelValuePair} from "../../types/common";
 import {GameTypeRequestParams} from "../../types/enums/game-enums";
 import CasinoGamesWrapper from "../../components/casino-games-wrapper/CasinoGamesWrapper";
 import './SlotGamesPage.scss';
@@ -11,7 +8,6 @@ const INTERVAL_SECONDS = 10;
 
 const SlotGamesPage = () => {
     const { playerInfo, operatorToken } = useAppSelector((state) => state.authorization);
-    const {selectedGameFeatures, selectedLines} = useAppSelector((state) => state.gamesFilters.slotGamesFilters);
     const { currency } = playerInfo || false;
 
     const {data, error, isLoading} = useGetGamesQuery({ operatorToken, currency, type: GameTypeRequestParams.slots }, {
@@ -19,53 +15,13 @@ const SlotGamesPage = () => {
         skip: !currency
     });
 
-    const [allGames, setAllGames] = useState<IGameType[]>([]);
-    const [featuredGames, setFeaturedGames] = useState<IGameType[]>([]);
-
-    // set featured games only on API data change
-    useEffect(() => {
-        const filteredFeaturedGamesData = data?.filter((el: IGameType) => el.isFeatured);
-        setFeaturedGames(filteredFeaturedGamesData)
-    }, [data]);
-
-    // set all games filtered data on data and filters data changed from the slice
-    useEffect(() => {
-        const [minimumLines, maximumLines] = getSelectedLineValues(selectedLines);
-        const filteredAllGamesData = filterAllGamesData(data, maximumLines, minimumLines, selectedGameFeatures);
-
-        setAllGames(filteredAllGamesData);
-    }, [data, selectedGameFeatures, selectedLines]);
-
     if (error) {
         return (<div className="error">Error fetching games, please try again later!</div>)
     }
     return(<div className="slot-games-wrapper">
-        <CasinoGamesWrapper name="Featured games" games={featuredGames} isFeatured areSlotGames isLoading={isLoading} />
-        <CasinoGamesWrapper name="Slots" games={allGames} hasFilter areSlotGames isLoading={isLoading} />
+        <CasinoGamesWrapper name="Featured games" games={data} isFeatured areSlotGames isLoading={isLoading} />
+        <CasinoGamesWrapper name="Slots" games={data} hasFilter areSlotGames isLoading={isLoading} />
     </div>)
 };
 
 export default SlotGamesPage;
-
-// these functions are currently staying in this file since they are not reused anywhere else,
-// so having them here would keep the code base cleaner
-const filterAllGamesData = (allGamesData: IGameType[], maximumLines: number, minimumLines: number, selectedGameFeatures: ILabelValuePair[]) => {
-    return allGamesData?.filter((el: IGameType) =>
-        el.slotData.linesCount >= minimumLines &&
-        el.slotData.linesCount <= maximumLines &&
-        (selectedGameFeatures.length === 0 || el.slotData.tags.some(tag => selectedGameFeatures.map(gf => gf.value).includes(tag))));
-}
-
-const getSelectedLineValues = (selectedLines: string[]): [number, number] => {
-    const selectedLineValues = selectedLines[0]
-        ? selectedLines[0].split(/[->]/).filter(el => !isNaN(el)).filter(el => el).map(el => Number(el))
-        : [0, Infinity];
-    // in case we have >50 to keep the logic flow without braking it
-    if (selectedLineValues.length === 1) {
-        selectedLineValues.push(Infinity);
-    }
-    const minimumLines = selectedLineValues[0];
-    const maximumLines = selectedLineValues[1];
-
-    return [minimumLines, maximumLines];
-}
